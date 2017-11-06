139c2f31d61dc1b78f503d233527f8a4345713b2
Dylan Heermann <dylan.heermann@yahoo.com>
2017-11-06 11:49:14 -0500

    Module 3 done

--- restaurantRater/Controllers/RestaurantController.cs
+++ restaurantRater/Controllers/RestaurantController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using restaurantRater.Models;
 
 namespace restaurantRater.Controllers
 {
     public class RestaurantController : Controller
     {
+        private RestaurantDBContext db = new RestaurantDBContext();
+
+
         // GET: Restaurant
         public ActionResult Index()
         {
-            return View();
+            return View(db.Restaurants.ToList());
         }
     }
 }
