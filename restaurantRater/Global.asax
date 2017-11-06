74c6f439d01d4f0b11b406863f0a30487d3c9d25
Dylan Heermann <dylan.heermann@yahoo.com>
2017-11-06 10:25:14 -0500

    first commit

---
+++ restaurantRater/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="restaurantRater.MvcApplication" Language="C#" %>
---
+++ restaurantRater/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(restaurantRater.Startup))]
+namespace restaurantRater
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
